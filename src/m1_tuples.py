47926e588ffa690dd8f381c81f6dc357bc20e613
michaelkuznicki <mkuznicki@franciscan.edu>
2024-02-27 10:13:52 -0500

    Wrote m1-m2

---
+++ src/m1_tuples.py
@@ -0,0 +1,79 @@
+###############################################################################
+# TODO: 1. (2 pts)
+#   
+#   This module is going to look very similar to m1 in the Session 10 Coding
+#   Exercises, but this time we will use tuples instead of lists.
+#
+#   For this _TODO_, simply create a tuple called recipes that will have a
+#   list of strings that represent different recipes of different meals you
+#   might make. Choose whichever meals you like, but make sure you have at
+#   least 4 meals in your list.
+#
+#   Once you have created the tuple, make sure to print out the tuple.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 2. (2 pts)
+#   
+#   For this _TODO_, write a line of code that accesses the *second* item in the
+#   tuple (remember the index of the first item is 0) and prints the item. Make
+#   sure you do NOT create a new tuple, but actually use the original.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 3. (2 pts)
+#   
+#   For this _TODO_, write some code that changes the *third* item to a
+#   different meal that you don't already have in your tuple. Once you have
+#   done this, print the tuple. Remember that tuples are immutable (can't be
+#   changed) so you will need to convert it to a list, do your modification,
+#   and convert it back again.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 4. (2 pts)
+#   
+#   For this _TODO_, write a some code that adds a recipe to the end of
+#   the tuple. Once you have done this, print the tuple. Remember that tuples
+#   are immutable (can't be changed) so you will need to convert it to a list,
+#   do your modification, and convert it back again.
+#
+#   NOTE: Your solution should work for any tuple of any length. DO NOT use
+#   indexes in your solution.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 5. (2 pts)
+#   
+#   For this _TODO_, write some code that removes the last item from the
+#   tuple. Once you have done this, print the tuple. Remember that tuples are
+#   immutable (can't be changed) so you will need to convert it to a list, do
+#   your modification, and convert it back again.
+#
+#   NOTE: Your solution should work for any tuple of any length. DO NOT use
+#   indexes or reference a specific item in your solution.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
+
+###############################################################################
+# TODO: 6. (2 pts)
+#
+#   In the reading, you saw a concept called unpacking a tuple. If you don't
+#   remember what this is, go back an look at the reading. Once you have done
+#   that, you should come back to this _TODO_.
+#
+#   For this _TODO_, write a line of code that unpacks your tuple into a
+#   variable for each item in the tuple as it currently is. Once you have done
+#   this, print each variable.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
---
+++ src/m2_sequences.py
@@ -0,0 +1,31 @@
+names = ["Francis", "Elizabeth", "Margaret", "Catherine", "Anthony", "Andrew", "Walter"]
+
+###############################################################################
+# TODO: 1. (4 pts)
+#
+#   For this _TODO_, we are going to take a look at lists again to help us
+#   understand sequences.
+#
+#   For this _TODO_, using the list of names from above, do all the steps below
+#   in order. Notice as you go how the list is changing (number of items and
+#   order). Lists are an example of sequences that can be modified, but they
+#   keep the attributes of finite and ordered.
+#
+#       1. Write a line of code that prints the list.
+#       2. Write a line of code that prints the length of the list.
+#       3. Write a line of code to add another name to the list. You pick a
+#          name.
+#       4. Write a line of code that prints the list now.
+#       5. Write a line of code that prints the length of the list now.
+#       6. Write a line of code that sorts the list in alphabetical order.
+#
+#          HINT: there is a method for sorting a list called sort(). So, if you
+#                wanted to sort a list called people, you would use it like
+#                this:
+#                   people.sort()
+#
+#       7. Write a line of code that prints the list now.
+#       8. Write a line of code that prints the length of the list now.
+#
+#   Once you have done this, then change the above _TODO_ to DONE.
+###############################################################################
